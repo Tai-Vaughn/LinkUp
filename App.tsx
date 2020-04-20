a60536d5f19d0968ad9ac0890101f9ae72b627c5
jodideer <jodideer18@gmail.com>
2020-04-19 19:23:58 -0500

    blue background

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.tsx to start working on your app!');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title = "Change text" onPress={() => setOutputText('The text has been changed!')} />
     </View>
   );
 }
@@ -12,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'steelblue', /*options: steelblue, cornflowerblue*/
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomColor: 'white',
+    borderBottomWidth: 10,
+    borderRadius: 20,
   },
 });
+
+

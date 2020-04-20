1c94121f4eb0f4124ba0b10edc5b9cc959389d7d
jodideer <jodideer18@gmail.com>
2020-04-19 19:37:58 -0500

    nothing new

---
+++ homescreen.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.tsx to start working on your app!');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title = "Change text" onPress={() => setOutputText('The text has been changed!')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'steelblue', /*options: steelblue, cornflowerblue*/
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: 'white',
+    borderBottomWidth: 10,
+    padding: -900,
+  },
+});

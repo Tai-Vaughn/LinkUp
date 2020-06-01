51d43a45a6b230b3406272af12b73babec46074f
Tai-Vaughn White <Isamutai200@gmail.com>
2020-06-01 14:42:49 -0500

    starting navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.tsx to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title = "Change text" onPress={() => setOutputText('The text has been changed!')} />
-    </View>
+    <NavigationContainer>
+    {/* Rest of your app code */}
+  </NavigationContainer>
   );
 }
 

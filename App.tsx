822653561e624cbacb37c90a2ecda4949788b849
Tai-Vaughn White <Isamutai200@gmail.com>
2020-06-01 15:19:48 -0500

    login and registration navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import 'react-native-gesture-handler';
+import login from './components/Login/LoginScreen';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
+import {createStackNavigator} from '@react-navigation/stack'
 import { StyleSheet, Text, View, Button} from 'react-native';
 
+
+const Stack = createStackNavigator(); 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="login" component={login}/>
+      </Stack.Navigator>
     {/* Rest of your app code */}
   </NavigationContainer>
   );

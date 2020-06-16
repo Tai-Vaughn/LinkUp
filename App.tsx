d8a1f716444e83eba21b98f1ddcbf1969880013e
Tai-Vaughn White <Isamutai200@gmail.com>
2020-06-16 11:47:42 -0500

    profile screen set up

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import {useState} from 'react';
 
 import LoginScreen from './components/Login/LoginScreen';
 import regis from './components/CreateAccount/CreateAccountScreen';
-import HomeScreen from './components/HomeScreen/HomeScreen'
+import HomeScreen from './components/HomeScreen/HomeScreen';
+import ProfileScreen from './components/Profile/Profile';
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,6 +13,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
+import { View, Text} from 'react-native';
 
 const getFonts = () => Font.loadAsync({
   'righteous': require('./components/fonts/Righteous-Regular.ttf')
@@ -24,17 +26,22 @@ export default function App() {
   const [islogedin , setIsLoggedin] = React.useState (true);
   const [JWT_Token , setToken] =React.useState(null);
   
+  
   if (fontsLoaded){
   return (
     <NavigationContainer>
-        {}
-      <AuthStack.Navigator>
-          <AuthStack.Screen name=" " component={HomeScreen}/>
-          <AuthStack.Screen name="login" component={LoginScreen} options= {{ title: ""}}/>
-          <AuthStack.Screen name="registration" component={regis} options={{ title : ""}}/>
-        </AuthStack.Navigator>  
-   
-  </NavigationContainer>
+        {JWT_Token ? (
+          <ProfileStack.Navigator> 
+            <ProfileStack.Screen name="Proffile" component={ProfileScreen} options= {{ title: ""}}/>
+          </ProfileStack.Navigator> 
+        ) : (
+          <AuthStack.Navigator>
+            <AuthStack.Screen name=" " component={HomeScreen}/>
+            <AuthStack.Screen name="login" component={LoginScreen} options= {{ title: ""}}/>
+            <AuthStack.Screen name="registration" component={regis} options={{ title : ""}}/>
+          </AuthStack.Navigator> 
+        )}
+    </NavigationContainer>
   );
   }
 
---
+++ components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Image, Text} from 'react-native';
+
+export default class Profile extends React.Component {
+    
+    render(){
+        return (
+            <View>
+                <Text>
+                    I'm working!
+                </Text>
+            </View>
+        )
+    }
+}

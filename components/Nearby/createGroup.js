1b8b4e3dee81334db2faf32165f6d11b4bba5bc8
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-17 18:51:40 -0500

    group size

--- components/Nearby/createGroup.js
+++ components/Nearby/createGroup.js
@@ -7,9 +7,11 @@ import {person} from './Person';
 import {ViewGroups} from './viewGroups'
 import nearbyPersons from "./nearbyPersons";
 import newId from './newId';
-
+let groupsList=[];
+//make an array of groups? when group count reach 20, stop and create a new group 
 //should be made from a list of active persons? 
 export default class CreateGroup extends Component{
+    
     constructor() {
         this.group = [], //array of persons that have the same path
         this.id=newId(),
@@ -18,31 +20,32 @@ export default class CreateGroup extends Component{
         this.destination = getPoints.end;
         this.path=path;
     };
+    
     addParty(person) {
         let i = this.path.length;
         let j=0;
-
-    //adds person to group if they have selected the same destination.
-    if (this.start===person.start && this.destination === person.end){
-        this.group.push(person.name);
-    }else{
-        //if there are other users who also follow the same path as the user
-        for(this.path[j];this.path[j]<i; j++){
-            //takes a point from the path and compares
-            if (this.path.point === person.path.point) {
-                this.group.push(person.name);
-                //person added to group if point matched
-            } 
-        }
-        //or a while loop
-        /*while (this.path[i] !== 0) {
-            if (this.path.point === person.path.point) {
-                this.group.push(person.name);
-                //person added to group if point matched
+        
+    //adds person to group if they have selected the same destination.  
+    //max for a group is 20, let it be 19 persons so the person can add themself to the group
+    while (this.group.length<19){
+        if (this.start===person.start && this.destination === person.end){
+            this.group.push(person.name);
+        }else{
+            //if there are other users who also follow the same path as the user
+            for(this.path[j];this.path[j]<i; j++){
+                //takes a point from the path and compares
+                if (this.path.point === person.path.point) {
+                    this.group.push(person.name);
+                    //person added to group if point matched
+                } 
             }
-            i--;
-        }*/
+        }
     }
+    groupsList.push(this.group);
+    //reset group counter and continue generating groups?
+}
+
+    
     getMembersHandler = () => {
 
     }
@@ -69,3 +72,11 @@ const styles = StyleSheet.create({
         },
     });
  
+            //or a while loop
+            /*while (this.path[i] !== 0) {
+                if (this.path.point === person.path.point) {
+                    this.group.push(person.name);
+                    //person added to group if point matched
+                }
+                i--;
+            }*/

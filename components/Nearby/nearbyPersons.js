dcb0f1817460b4f13adfe6afeed9068b3737a2a6
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-17 08:40:34 -0500

    nearby for updated

--- components/Nearby/nearbyPersons.js
+++ components/Nearby/nearbyPersons.js
@@ -6,25 +6,25 @@ import ActivePerson from "./ActivePerson";
 import person from './Person';
 import CreateGroup from "./createGroup";
 
-const nearbyPersons = props => {
+const nearbyPersons = (path) => {
      //if the destination entered by the user is a point along a longer route that other users are currently travelling
 let start = getPoints.point1;
 let end = getPoints.point2;
-const path = {path};//need to pass actual path and not empty array
+const {path} = path;//need to pass actual path and not empty array
 let i = path.length;
-let j=0;
+
 let person={person};
 let activeperson;
 //active person should be a variable person, compared with the path to create a list of active persons
-
-for(this.path[j];this.path[j]<i; j++){
+    for(let j=0;j<i; j++){
     //need to separate by users to compare by username
     //adds a person if they have a start or end point that matches the path
-   
-    if (path[j] === start || path[j] === end) {
+        const pathNow=path[j];
+    if (pathNow === start || pathNow === end) {
         activeperson=ActivePerson.setStatusHandler(person);
         CreateGroup.addParty(activeperson);
     }
+
 }
 /* replaced while with for loop
 while (path[i] !==0) {

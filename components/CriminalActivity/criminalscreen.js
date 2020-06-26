86a7d40b9431c66c009c6db6880a21d73b0adb21
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-25 23:39:26 -0500

    handlers

--- components/CriminalActivity/criminalscreen.js
+++ components/CriminalActivity/criminalscreen.js
@@ -5,32 +5,113 @@ import {globalStyles} from '../Styles';
 
 class CriminalScreen extends React.Component{
     assaultHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Aggravated Assault</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    breakHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Break-In</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    kidnapHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Kidnapping/Attempted Kidnapping'</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    robHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Robbery/Attempted Robbery</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    saHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Sexual Assault/Rape</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    shootHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Shooting</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
+        
+    }
+    theftHandler=()=>{
+        return(
+            <ScrollView>
+                <View>
+                    <Text>Theft</Text>
+                    <Text>{description}</Text>
+                </View>
+            </ScrollView>
+            
+        )
         
     }
   render(){
       return (
         <ScrollView>
-          <View style={globalStyles.container}>
+            <View style={globalStyles.container}>
 
-          <View style={globalStyles.logoContainer}>
-            <Image 
-            style={globalStyles.logo}
-            source={require('../../static/LinkUp.png')}/>
-            <Text style={globalStyles.name}>RECENT CRIMINAL ACTIVITY</Text>
-          </View>  
+            <View style={globalStyles.logoContainer}>
+                <Image 
+                style={globalStyles.logo}
+                source={require('../../static/LinkUp.png')}/>
+                <Text style={globalStyles.name}>RECENT CRIMINAL ACTIVITY</Text>
+            </View>  
 
-          <View>
-              <Button title='Aggravated Assault' onPress={this.assaultHandler}/>
-              <Button title='Break-In'/>
-              <Button title='Kidnapping/Attempted Kidnapping'/>
-              <Button title='Robbery/Attempted Robbery'/>
-              <Button title='Sexual Assault/Rape'/>
-              <Button title='Shooting'/>
-              <Button title='Theft'/>
-          </View>
-          
-          </View>
-          </ScrollView>
+            <View>
+                <Button title='Aggravated Assault' onPress={this.assaultHandler}/>
+                <Button title='Break-In'onPress={this.breakHandler}/>
+                <Button title='Kidnapping/Attempted Kidnapping'onPress={this.kidnapHandler}/>
+                <Button title='Robbery/Attempted Robbery'onPress={this.robHandler}/>
+                <Button title='Sexual Assault/Rape'onPress={this.saHandler}/>
+                <Button title='Shooting'onPress={this.shootHandler}/>
+                <Button title='Theft'onPress={this.theftHandler}/>
+            </View>
+            
+            </View>
+        </ScrollView>
       )
   }
 }

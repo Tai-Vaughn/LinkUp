fc9eed73c746e6f3c3bfb3c52beb85beee5d03ff
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-27 12:45:42 -0500

    call alert on profile

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,13 +1,21 @@
 import React  from 'react';
 import { View , Text} from 'react-native';
+import CriminalScreen from '../CriminalActivity/criminalscreen';
 
 class StartMenue extends React.Component {
+    callAlerts=()=>{
+        <View>
+            <CriminalScreen />
+        </View>
+
+        }
     render() {
         return (
             <View>
-                <Text>
-                    im working wefw dasdas 
-                </Text>
+                 <Button
+                    name='Alert'
+                    onPress={() => {
+                        this.callAlerts();}} />
             </View>
         ); 
     }

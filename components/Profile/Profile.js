e1b51c3e06a2974f3d088b415f9a32cb2c0de9d8
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-27 12:50:12 -0500

    update profile

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -11,12 +11,44 @@ export default class Profile extends React.Component {
         }
     render() {
         return (
-            <View>
-                 <Button
+            <View style={styles.container}>
+                <Text style={styles.text}>Profile Page</Text>
+                <View>
+                    <Text>{firstname} {lastname}</Text>
+                </View>
+                <View >
+                <Image source={require('../../static/LinkUp.png')}/>
+                </View>
+                <View>
+                    <Text>{username}</Text>
+                </View>
+                <Text>Menu:</Text>
+                <View style={globalStyles.button}>
+                    <Button
+                        title='Map'
+                    />
+                </View>
+
+                <View style={globalStyles.button}>
+                    <Button
+                        title='Find Route'
+        
+                    />
+                </View>
+                <View style={globalStyles.button}>
+                    <Button
+                        title='Friends'
+        
+                    />
+                </View>
+                <View style={globalStyles.button}>
+                <Button
                     name='Alert'
                     onPress={() => {
                         this.callAlerts();}} />
+                </View>
+                
             </View>
         ); 
-                    }
+    }
 }

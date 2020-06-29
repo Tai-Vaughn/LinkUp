c7b4c07a94324539dfa5858cfed2c09c9f60654e
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-29 14:56:32 -0500

    button1

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,13 +1,19 @@
 import React  from 'react';
-import { View , Text} from 'react-native';
+import { View , Text, Button} from 'react-native';
 
 class StartMenue extends React.Component {
     render() {
         return (
             <View>
                 <Text>
-                    im working wefw dasdas 
+                    Profile Page
                 </Text>
+                <Text>John Doe</Text>
+                <Text>jdoe</Text>
+                <View>
+                    <Text> Menu:</Text>
+                    <Button title='Map'/>
+                </View>
             </View>
         ); 
     }

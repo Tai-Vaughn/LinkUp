e84ed1d7cffe6cf7fb82c3048b7d5a9eada2a645
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-29 15:11:02 -0500

    find route

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,7 +1,13 @@
 import React  from 'react';
 import { View , Text, Button} from 'react-native';
+import FindRouteScreen from '../FindRoute/FindRouteScreen';
 
 class StartMenue extends React.Component {
+    callRoute(){
+        <View>
+            <FindRouteScreen />
+        </View>
+    }
     render() {
         return (
             <View>
@@ -13,6 +19,9 @@ class StartMenue extends React.Component {
                 <View>
                     <Text> Menu:</Text>
                     <Button title='Map'/>
+                    <Button title='Find Route' onPress={() =>{this.callRoute();}}/>
+                    <Button title='Alert'/>
+                    <Button title='Friends'/>
                 </View>
             </View>
         ); 

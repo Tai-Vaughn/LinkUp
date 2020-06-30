e729719bd70363000d46e4347285300d639fb0f0
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-29 22:45:18 -0500

    getter

--- components/Alerts/Alerter.js
+++ components/Alerts/Alerter.js
@@ -4,6 +4,28 @@ import {globalStyles} from '../Styles';
 
 
 class Alerter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+           isLoading:true,
+            dataSource:null,
+        }
+    }
+    //Get from database
+    componentDidMount(){
+        return fetch('https://linkupcapstone.herokuapp.com/users/reports')
+        .then((response)=>response.json())
+        .then((responseJson)=>{
+            this.setState({
+                isLoading:false,
+                dataSource:responseJson.reports,
+
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        });
+    }
     assaultHandler=()=>{
         for(let i=0; i<4;i++){
             if(report.type==="Aggravated Assault"){
@@ -24,27 +46,45 @@ class Alerter extends React.Component{
         
     }
     render() {
-        return (
-            <ScrollView>
-            <View style={styles.container}>
-                <View style={globalStyles.logoContainer}>
-                <Image 
-                style={globalStyles.logo}
-                source={require('../../static/LinkUp.png')}/>
-                <Text style={styles.text}>RECENT CRIMINAL ACTIVITY</Text>
-            </View> 
-            <View style={globalStyles.button}>
-                <Button title='Aggravated Assault' onPress={this.assaultHandler}/>
-                <Button title='Break-In'onPress={this.breakHandler}/>
-                <Button title='Kidnapping/Attempted Kidnapping'onPress={this.kidnapHandler}/>
-                <Button title='Robbery/Attempted Robbery'onPress={this.robHandler}/>
-                <Button title='Sexual Assault/Rape'onPress={this.saHandler}/>
-                <Button title='Shooting'onPress={this.shootHandler}/>
-                <Button title='Theft'onPress={this.theftHandler}/>
-            </View> 
-            </View>
-            </ScrollView>
-        ); 
+        if(this.state.isLoading){
+            return(
+                <View>
+                    <Text>
+                        Profile Page
+                    </Text>
+                </View>
+            )
+        }else{
+            let reports=this.state.dataSource.map((val,key)=>{
+                return <View key={key}><Text>{val.title}: {val.description}</Text></View>
+            });
+//put return statement here and call users variable
+            return (
+                <ScrollView>
+                <View style={styles.container}>
+                    <View style={globalStyles.logoContainer}>
+                    <Image 
+                    style={globalStyles.logo}
+                    source={require('../../static/LinkUp.png')}/>
+                    <Text style={styles.text}>RECENT CRIMINAL ACTIVITY</Text>
+                </View> 
+                <View>
+                    <Text>{reports}</Text>
+                </View>
+                <View style={globalStyles.button}>
+                    <Button title='Aggravated Assault' onPress={this.assaultHandler}/>
+                    <Button title='Break-In'onPress={this.breakHandler}/>
+                    <Button title='Kidnapping/Attempted Kidnapping'onPress={this.kidnapHandler}/>
+                    <Button title='Robbery/Attempted Robbery'onPress={this.robHandler}/>
+                    <Button title='Sexual Assault/Rape'onPress={this.saHandler}/>
+                    <Button title='Shooting'onPress={this.shootHandler}/>
+                    <Button title='Theft'onPress={this.theftHandler}/>
+                </View> 
+                </View>
+                </ScrollView>
+            ); 
+        }
+        
     }
 }
 const styles = StyleSheet.create({

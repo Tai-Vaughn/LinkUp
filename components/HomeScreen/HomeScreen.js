f1564a2837950f1c55a3b17b2c2c2a5e08a050b4
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-30 00:32:56 -0500

    omport

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Image, Button, Text} from 'react-native';
+import {StyleSheet, View, Image, Button, Text, TouchableOpacity} from 'react-native';
 import {globalStyles} from '../Styles'; 
 
 export default function HomeScreen ({navigation}) {

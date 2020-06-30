895172a1881a11b2af04de1c76b98ccdba438817
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-29 22:11:11 -0500

    srtyle

--- components/Alerts/Alerter.js
+++ components/Alerts/Alerter.js
@@ -6,12 +6,12 @@ import {globalStyles} from '../Styles';
 class Alerter extends React.Component{
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <View style={globalStyles.logoContainer}>
                 <Image 
                 style={globalStyles.logo}
                 source={require('../../static/LinkUp.png')}/>
-                <Text style={globalStyles.name}>RECENT CRIMINAL ACTIVITY</Text>
+                <Text style={styles.text}>RECENT CRIMINAL ACTIVITY</Text>
             </View> 
             <View>
                 <Button title='Aggravated Assault' onPress={this.assaultHandler}/>
@@ -26,5 +26,21 @@ class Alerter extends React.Component{
         ); 
     }
 }
-
+const styles = StyleSheet.create({
+    container: {
+       backgroundColor: 'steelblue',
+       padding: 50,
+       justifyContent: 'center',
+       alignItems: 'center',
+       flex: 1
+   },
+ 
+   text:{
+     color: 'white',
+     fontSize: 40,
+     marginBottom: 90,
+     fontFamily: 'righteous'
+   }
+ 
+ });
 export default Alerter;

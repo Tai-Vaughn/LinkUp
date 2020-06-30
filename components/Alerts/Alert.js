1b0b8c38d3fa7460676a7aedbd4283694ee232b8
Nat Keane <ntlkeane.nk@gmail.com>
2020-06-29 21:52:17 -0500

    alert

--- components/Alerts/Alert.js
+++ components/Alerts/Alert.js
@@ -7,7 +7,12 @@ class Alert extends React.Component{
     render() {
         return (
             <View>
+                <View style={globalStyles.logoContainer}>
+                <Image 
+                style={globalStyles.logo}
+                source={require('../../static/LinkUp.png')}/>
                 <Text style={globalStyles.name}>RECENT CRIMINAL ACTIVITY</Text>
+            </View>  
             </View>
         ); 
     }

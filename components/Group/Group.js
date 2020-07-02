754554c024efe683ecd1589b44f973fff94e848c
Nat Keane <ntlkeane.nk@gmail.com>
2020-07-02 18:29:15 -0500

    Revert "remove members"
    This reverts commit f8dd5cf5758a3e985c9835a09d6423daac748985.

--- components/Group/Group.js
+++ components/Group/Group.js
@@ -1,9 +1,8 @@
 import React  from 'react';
-import { FlatList, StyleSheet, Text, Button, View, ScrollView, TouchableOpacity} from "react-native";
+import { FlatList, StyleSheet, Text, Button, View, ScrollView, TouchableHighlight} from "react-native";
 import {globalStyles} from '../Styles'; 
 import * as DataService from '../Service/DataService'
 import { date } from 'yup';
-
 const group = [
     {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
@@ -85,11 +84,7 @@ class Group extends React.Component {
                     <View>
                     <Text style={styles.nullgroup}>No Groups Available.</Text>
                     </View>
-                    <View style={globalStyles.button}>
-                        <Button title='Create Group'
-                        onPress = {() => this.addMemberHandler}/>
-                        <FindRouteScreen />
-                </View>
+                    
                 </View>
             );
     }else{
@@ -104,10 +99,10 @@ class Group extends React.Component {
                             keyExtractor={item=> item.id}
                             data={group}
                             renderItem={({item}) => (
-                            <TouchableOpacity
-                            onPress={() => <viewGroupHandler data item />}>
+                            <TouchableHighlight
+                            onPress={() => this.viewGroupHandler(group, item)}>
                             <Text style={styles.listItems}>{item.name}</Text>
-                            </TouchableOpacity>
+                            </TouchableHighlight>
                     )}/>
                 
             </View> 

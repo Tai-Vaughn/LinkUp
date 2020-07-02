279405a5c66f231ee24382c8b7f3ec39a56e3881
Nat Keane <ntlkeane.nk@gmail.com>
2020-07-01 23:10:07 -0500

    group

--- components/Group/Group.js
+++ components/Group/Group.js
@@ -1,29 +1,72 @@
 import React  from 'react';
-import { View , Text, Button, StyleSheet} from 'react-native';
+import { FlatList, StyleSheet, Text, Button, View, ScrollView, TouchableOpacity} from "react-native";
+
+let group=['0:lol','1:Amy', '2:Joe'];
 
 class Group extends React.Component {
+    viewGroupHandler=()=>{
+        let i=0;
+        //let group=[];
+        //groupsList= DataService.groups$.subscribe();
+        let j=group.length;
+        if (group===0){
+            console.warn('No Groups.')
+                return;
+        }else{
+
+        }
+    }
+    
     render() {
+        let i=0;
+        let j=group.length;
+    if (j===0){
+        console.warn('No Groups.')
+            return;
+    }else{
+        for(i;i<j;i++){
+            //if groups is populated display a list of persons/groups
+            //list group names
         return (
             <View style={styles.container}>
-                <Text>
-                    Groups
-                </Text>
+               <SafeAreaView style={styles.container}>
+                <FlatList
+                            keyExtractor={item=> item.id}
+                            data={group}
+                            renderItem={item => (
+                            <View style={styles.listItems} >
+                                <TouchableOpacity onPress={this.showGroupMembersHandler}>
+                                    <Text>Available Groups: {item.groupName}</Text>
+                                </TouchableOpacity>
+                                
+                            </View>)}/>
+                </SafeAreaView>
+                    
                 <View style={globalStyles.button}>
                             <Button 
-                            title='Search' 
-                            onPress={() => DataService.getGroups()}
-                            onPress={props.handleSubmit}
-                            />
+                            title='Search'/>
                 </View>
             </View>
-        ); 
+            
+            
+            );}
+                            }
     }
 }
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: 'steelblue',
+       padding: 50,
+       justifyContent: 'center',
+       alignItems: 'center',
         flex: 1,
-        paddingTop: 22
     },
+    text:{
+        color: 'white',
+        fontSize: 20,
+        marginBottom: 90,
+        fontFamily: 'righteous'
+      },
     listItems: {
         padding: 10,
             fontSize: 18,
@@ -31,3 +74,27 @@ const styles = StyleSheet.create({
         },
     });
 export default Group;
+
+/*return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Groups</Text>
+                <SafeAreaView>
+                <FlatList
+                            keyExtractor={item=> item.id}
+                            data={group}
+                            renderItem={item => (
+                            <View style={styles.listItems} >
+                                <TouchableOpacity onPress={this.viewGroupHandler}>
+                                    <Text style={styles.text}>Available Groups: {item.groupName}</Text>
+                                </TouchableOpacity>
+                                
+                            </View>)}/>
+                </SafeAreaView>
+                    
+                <View style={globalStyles.button}>
+                            <Button 
+                            title='Search' 
+                            />
+                </View>
+            </View>
+        ); */

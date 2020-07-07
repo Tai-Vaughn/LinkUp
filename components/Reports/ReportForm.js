46ee8ce29541592015b5953f02bba64c68e176b5
Nat Keane <ntlkeane.nk@gmail.com>
2020-07-07 14:40:58 -0500

    schema

--- components/Reports/ReportForm.js
+++ components/Reports/ReportForm.js
@@ -43,7 +43,7 @@ export default class ReportForm extends Component {
                         Description: '',
                         Time: ''
                     }}
-                   // validationSchema={ReportFormSchema}
+                    validationSchema={ReportFormSchema}
                     onSubmit={(values, action) => {
                         action.resetForm();
                         console.log(values);
@@ -64,11 +64,8 @@ export default class ReportForm extends Component {
                             <Picker
                                 style={styles.input}
                                 selectedValue={this.state.Type}
-                                onValueChange={(itemValue) => {
-                                    setFieldValue('Type', itemValue)
-                                    this.setState({Type: itemValue})
-                                }}>
-                               // onValueChange={(itemValue) =>this.setState({ Type: itemValue })} 
+                                onValueChange={(itemValue) =>this.setState({ Type: itemValue })} >
+                               
                             
                                 <Picker.Item label='Type of Crime' value='' />
                                 <Picker.Item label='Aggravated Assault' value='Aggravated Assault' />
